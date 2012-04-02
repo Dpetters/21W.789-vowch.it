3d6aeff53decec073ccfe69f6ac1400b1761eb48
Dmitrij Petters <dpetters@mit.edu>
2012-04-01 22:45:38 -0400

    code commit

---
+++ src/it/vowch/android/CommunityWallActivity.java
@@ -0,0 +1,14 @@
+package it.vowch.android;
+
+import it.vowch.android.R;
+import android.app.Activity;
+import android.os.Bundle;
+
+public class CommunityWallActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
