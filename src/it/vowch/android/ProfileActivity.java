4505129f9c7b4394c93cdc9b8779326e6e0f85ac
unknown <bjain@bjain-PC.(none)>
2012-04-02 23:01:13 -0400

    popup

--- src/it/vowch/android/ProfileActivity.java
+++ src/it/vowch/android/ProfileActivity.java
@@ -2,16 +2,49 @@ package it.vowch.android;
 
 import android.app.ListActivity;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.PopupWindow;
+import android.widget.TextView;
 
 public class ProfileActivity extends ListActivity {
     /** Called when the activity is first created. */
+	protected Button testButton;
+	protected PopupWindow popup;
+    protected LinearLayout layout;
+    protected boolean click = true;
+    protected TextView tv;
+	
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.profile);
+        
+        testButton = (Button)findViewById(R.id.testButton);
+        popup = new PopupWindow(this);
+        tv = (TextView)findViewById(R.id.reputation_level);
+        
+        testButton.setOnClickListener(new OnClickListener(){
+        	@Override
+        	public void onClick(View v){
+        		if (click) {
+        	         popup.showAtLocation(layout, Gravity.BOTTOM, 10, 10);
+        	         popup.update(50, 50, 300, 80);
+        	         click = false;
+        	        } else {
+        	         popup.dismiss();
+        	         click = true;
+        	        }
+        	}
+
+
+        });
     }
 
     @Override
